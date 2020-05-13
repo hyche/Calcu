718e17cc1da766b0a5efa54b96bddfc31d56a429
Che Vu Gia Hy <cvghy116@gmail.com>
2020-05-13 19:20:03 +0700

    backend: Add handling CORS

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 from .core import evaluate
 
+
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    expression = request.json['format']
+    expression = request.json['formula']
     result = evaluate(expression)
     return jsonify({'result': str(result)})
